8369b19d04db642e756b843236e6b1cdbc7c05e2
Jaime Escalona <jaescalo@akamai.com>
2021-04-27 17:25:17 -0600

    Inline JSON from local file

--- main.js
+++ main.js
@@ -9,11 +9,15 @@ import { createResponse } from 'create-response';
 import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
 import { FindAndReplaceStream } from 'find-replace-stream.js';
 import { logger } from 'log';
+import * as inline from './data/inline.json';
+
+const inlineJson = inline;
+logger.log(inlineJson);
 
 export function responseProvider (request) {
   // Get text to be searched for and new replacement text from Property Manager variables in the request object.
   const tosearchfor = "</body>";
-  const newtext = '\t' + `<data class="json-data" value='{"unicorns": "awesome","abc": [1, 2, 3],"careful": "to escape &#39; quotes"}'></data>` + '\n</body>';
+  const newtext = '\t' + `<data class="json-data" value='` + inlineJson + '></data>\n</body>';
   // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
   const howManyReplacements = 1;
 
