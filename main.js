3cb32c3382091fc97ab8dcbabd9ebc0c38e2ac01
Jaime Escalona <jaescalo@akamai.com>
2021-04-27 11:58:07 -0600

    Back to streamed non-buffered response

--- main.js
+++ main.js
@@ -1,65 +1,29 @@
 /*
 (c) Copyright 2020 Akamai Technologies, Inc. Licensed under Apache 2 license.
-Version: 0.1
-Purpose:  Modify an HTML streamed response by adding a script before the closing head tag.
-Repo: https://github.com/akamai/edgeworkers-examples/tree/master/response-manipulaton-stream
+Version: 1.1
+Purpose:  Modify an HTML streamed response by replacing a text string two times across the entire response.
 */
 
-import { ReadableStream, WritableStream } from 'streams';
+//import { ReadableStream, WritableStream } from 'streams';
 import { httpRequest } from 'http-request';
 import { createResponse } from 'create-response';
 import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
+import { FindAndReplaceStream } from 'find-replace-stream.js';
 import { logger } from 'log';
 
-class HTMLStream {
-  constructor () {
-    let readController = null;
-
-    const script = '<script>alert("added by EdgeWorker");</script>';
-    const tag = '</head>';
-
-    this.readable = new ReadableStream({
-      start (controller) {
-        readController = controller;
-      }
-    });
-
-    async function handleTemplate (text) {
-      const startIndex = text.indexOf(tag);
-      if (startIndex === -1) {
-        readController.enqueue(text);
-      } else {
-        readController.enqueue(text.substring(0, startIndex));
-        readController.enqueue(script);
-        readController.enqueue(text.substring(startIndex));
-      }
-    }
-
-    let completeProcessing = Promise.resolve();
-
-    this.writable = new WritableStream({
-      write (text) {
-        completeProcessing = handleTemplate(text, 0);
-      },
-      close (controller) {
-        completeProcessing.then(() => readController.close());
-      }
-    });
-  }
-}
-
-// When NetStorage is in use request.url = /cpcode/any-fwd-path-rewrites/actual-url
 export function responseProvider (request) {
+  // Get text to be searched for and new replacement text from Property Manager variables in the request object.
+  const tosearchfor = request.getVariable('PMUSER_EWSEARCH');
+  const newtext = request.getVariable('PMUSER_EWNEWTEXT');
+  // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
+  const howManyReplacements = 1;
+
   return httpRequest(`${request.scheme}://${request.host}/json-inline-demo`).then(response => {
     logger.log(request.url);
-    logger.log(response.text());
-    logger.log(response.json());
-    logger.log(response.getHeaders());
-    logger.log(response.body);
     return createResponse(
       response.status,
-      response.headers,
-      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new HTMLStream()).pipeThrough(new TextEncoderStream())
+      response.getHeaders(),
+      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, newtext, howManyReplacements)).pipeThrough(new TextEncoderStream())
     );
   });
 }
