e22299fd42568c5a1ec49599d4b90625b1d38265
Jaime Escalona <jaescalo@akamai.com>
2021-04-27 15:26:23 -0600

    Back to buffered

--- main.js
+++ main.js
@@ -4,25 +4,56 @@ Version: 1.1
 Purpose:  Modify an HTML streamed response by replacing a text string two times across the entire response.
 */
 
+import { ReadableStream, WritableStream } from 'streams';
 import { httpRequest } from 'http-request';
 import { createResponse } from 'create-response';
 import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
-import { FindAndReplaceStream } from 'find-replace-stream.js';
 import { logger } from 'log';
 
-export function responseProvider (request) {
-  // Get text to be searched for and new replacement text from Property Manager variables in the request object.
-  const tosearchfor = "</body>";
-  const newtext = `<data class="json-data" value='{"unicorns": "awesome","abc": [1, 2, 3],"careful": "to escape &#39; quotes"}'></data></body>`;
-  // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
-  const howManyReplacements = 1;
+class HTMLStream {
+  constructor () {
+    let readController = null;
 
+    // Search for the closing </body> tag and add some json-data
+    const script = `<data class="json-data" value='{"unicorns": "awesome","abc": [1, 2, 3],"careful": "to escape &#39; quotes"}'></data>`;
+    const tag = '</body>';
+
+    this.readable = new ReadableStream({
+      start (controller) {
+        readController = controller;
+      }
+    });
+
+    async function handleTemplate (text) {
+      const startIndex = text.indexOf(tag);
+      if (startIndex === -1) {
+        readController.enqueue(text);
+      } else {
+        readController.enqueue(text.substring(0, startIndex));
+        readController.enqueue(script);
+        readController.enqueue(text.substring(startIndex));
+      }
+    }
+
+    let completeProcessing = Promise.resolve();
+
+    this.writable = new WritableStream({
+      write (text) {
+        completeProcessing = handleTemplate(text, 0);
+      },
+      close (controller) {
+        completeProcessing.then(() => readController.close());
+      }
+    });
+  }
+}
+
+export function responseProvider (request) {
   return httpRequest(`${request.scheme}://${request.host}/json-inline-demo/index.html`).then(response => {
-    logger.log(request.url);
     return createResponse(
       response.status,
       response.getHeaders(),
-      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, newtext, howManyReplacements)).pipeThrough(new TextEncoderStream())
+      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new HTMLStream()).pipeThrough(new TextEncoderStream())
     );
   });
 }
